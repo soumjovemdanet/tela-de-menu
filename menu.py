66f2035418c147cab2739f05fc57597474b90486
soumjovemdanet <96385166+soumjovemdanet@users.noreply.github.com>
2022-01-26 20:25:33 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,17 @@
+import python 2,3
+
+	print('******************************')
+  print('* Esse menu feito por sou um  ')
+  print('* sou um jovem da net          ')
+	print('******************************')
+  
+                 
+        print('''  
+               1- Atualizar comandos essenciais termux
+               2- autualizar esse projeto 
+               
+       ''')        
+      
+os.system('apt-update')
+os.system('apt-upgrade')
+os.
